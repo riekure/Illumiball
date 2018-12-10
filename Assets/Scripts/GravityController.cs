2359d4131bc8f2bb8ab19187b5f051d4cea44553
riekure <s2darl0uvgia24@gmail.com>
2018-12-10 23:17:57 +0900

    add acceleration sensor

--- Assets/Scripts/GravityController.cs
+++ Assets/Scripts/GravityController.cs
@@ -20,17 +20,28 @@ public class GravityController : MonoBehaviour {
         // 重力ベクトルの初期化
         Vector3 vector = new Vector3();
 
-        // キー入力を検知してベクトルを設定
-        vector.x = Input.GetAxis("Horizontal");
-        vector.y = Input.GetAxis("Vertical");
-
-        // 高さ方向の判定はキーのzとする
+        // Unityエディタと実機で処理を分ける
         if (Input.GetKey("z"))
         {
-            vector.y = 1.0f;
+            // キー入力を検知してベクトルを設定
+            vector.x = Input.GetAxis("Horizontal");
+            vector.y = Input.GetAxis("Vertical");
+
+            // 高さ方向の判定はキーのzとする
+            if (Input.GetKey("z"))
+            {
+                vector.y = 1.0f;
+            }
+            else
+            {
+                vector.y = -1.0f;
+            }
         } else
         {
-            vector.y = -1.0f;
+            // 加速度センサーの入力をUnity空間の軸をマッピング
+            vector.x = Input.acceleration.x;
+            vector.z = Input.acceleration.y;
+            vector.y = Input.acceleration.z;
         }
 
         // シーンの重力を入力ベクトルの方向に合わせて変化させる
