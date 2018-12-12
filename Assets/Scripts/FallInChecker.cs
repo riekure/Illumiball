8cd17f06c5f0eabcdf176b7ded8ad8d3870221a4
riekure <s2darl0uvgia24@gmail.com>
2018-12-12 22:43:44 +0900

    add FallInChecker

---
+++ Assets/Scripts/FallInChecker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallInChecker : MonoBehaviour {
+
+    public Hole red;
+    public Hole blue;
+    public Hole green;
+
+    void onGUI()
+    {
+        string label = " ";
+
+        // すべてのボールが入ったらラベルを表示
+        if (red.InFallIn() && blue.InFallIn() && green.InFallIn())
+        {
+            label = "Fall in hole!";
+        }
+        GUI.Label(new Rect(0, 0, 100, 30), label);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
